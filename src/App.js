949e6aff72766155e5c23e184103b461544251ab
m4r-zero <50446006+chauneng@users.noreply.github.com>
2023-01-11 00:54:25 +0900

    [add] component style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div className='container'>
-        <div className='todoBlock'>
-          <div className='title'>
-            <h1>할 일 목록</h1>
-          </div>
-        </div>
-      </div>
-    );
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+  };
+
+  getStyle = () => {
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textdecoration: 'none',
+    }
   }
+
+	render() {
+		return (
+			<div className="container">
+				<div className="todoBlock">
+					<div className="title">
+						<h1>할 일 목록</h1>
+					</div>
+					<div style={this.getStyle()}>
+						<input type="checkbox" defaultChecked={false} />
+						공부하기
+						<button style={this.btnStyle}>x</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }

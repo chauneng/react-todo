7258d5ace53470710a29e869e03afc4f8cccce7c
m4r-zero <50446006+chauneng@users.noreply.github.com>
2023-01-25 18:26:31 +0900

    [add] change todo list title feature

--- src/components/list.js
+++ src/components/list.js
@@ -1,45 +1,113 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const List = React.memo(({ id, title, completed, todoData, setTodoData, provided, snapshot, handleClick }) => {
-  const handleCompleteChange = (id) => {
-    const newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
-  };
-  
-  return (
-    <div
-      key={id}
-      {...provided.draggableProps}
-      ref={provided.innerRef}
-      {...provided.dragHandleProps}
-      className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
-        } flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  border rounded`}
-    >
-      <div className="items-center">
-        <input
-          type="checkbox"
-          defaultChecked={completed}
-          onChange={() => handleCompleteChange(id)}
-        />
-        <span className={completed ? "line-through" : undefined}>
-          {title}
-        </span>
-      </div>
-      <div className="items-center">
-        <button
-          className="px-4 py-2 float-right"
-          onClick={() => handleClick(id)}
-        >
-          x
-        </button>
-      </div>
-    </div>
-  );
-});
+const List = React.memo(
+	({
+		id,
+		title,
+		completed,
+		todoData,
+		setTodoData,
+		provided,
+		snapshot,
+		handleClick,
+	}) => {
+		const [isEditing, setIsEditing] = useState(false);
+		const [editedTitle, setEditedTitle] = useState(title);
+
+		const handleCompleteChange = (id) => {
+			const newTodoData = todoData.map((data) => {
+				if (data.id === id) {
+					data.completed = !data.completed;
+				}
+				return data;
+			});
+			setTodoData(newTodoData);
+    };
+    
+    const handleEditChange = (event) => {
+      setEditedTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const newTodoData = todoData.map((elem) => {
+        if (elem.id === id) {
+          elem.title = editedTitle;
+        }
+        return elem;
+      })
+      setTodoData(newTodoData);
+      setIsEditing(false);
+    }
+
+		return isEditing ? (
+			<div
+				className={
+					"flex items-center justify-between w-full px-4 py-1 my-2 bg-gray-100 text-gray-600  border rounded"
+				}
+			>
+				<div className="items-center">
+					<form onSubmit={handleSubmit}>
+            <input
+              value={editedTitle}
+              className="w-full px-3 py-2 mr-4 text-gray-500 rounded"
+              onChange={handleEditChange}
+						/>
+					</form>
+				</div>
+				<div className="items-center">
+					<button
+						className="px-4 py-2 float-right"
+						onClick={() => setIsEditing(false)}
+					>
+						x
+					</button>
+          <button
+            onClick={handleSubmit}
+            className="px-4 py-2 float-right"
+            type='submit'
+					>
+						save
+					</button>
+				</div>
+			</div>
+		) : (
+			<div
+				key={id}
+				{...provided.draggableProps}
+				ref={provided.innerRef}
+				{...provided.dragHandleProps}
+				className={`${
+					snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"
+				} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  border rounded`}
+			>
+				<div className="items-center">
+					<input
+						type="checkbox"
+						defaultChecked={completed}
+						onChange={() => handleCompleteChange(id)}
+					/>
+					<span className={completed ? "line-through" : undefined}>
+						{title}
+					</span>
+				</div>
+				<div className="items-center">
+					<button
+						className="px-4 py-2 float-right"
+						onClick={() => handleClick(id)}
+					>
+						x
+					</button>
+					<button
+						className="px-4 py-2 float-right"
+						onClick={() => setIsEditing(true)}
+					>
+						edit
+					</button>
+				</div>
+			</div>
+		);
+	}
+);
 
 export default List;
